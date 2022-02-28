61e73cf48aa0fc9f1f04e86a72ce82b1057fe025
Jakub Drozdek <qooboos@gmail.com>
2022-02-28 11:52:28 +0100

    Resolve merge conflicts

--- beta/src/components/Search.tsx
+++ beta/src/components/Search.tsx
@@ -106,13 +106,8 @@ export const Search: React.FC<SearchProps> = ({
         type="button"
         className="hidden md:flex relative pl-4 pr-0.5 py-1 h-10 bg-secondary-button dark:bg-gray-80 outline-none focus:ring focus:outline-none betterhover:hover:bg-opacity-80 pointer items-center shadow-inner text-left w-full text-gray-30 rounded-lg align-middle text-sm"
         onClick={onOpen}>
-<<<<<<< HEAD
-        <IconSearch className="mr-3 align-middle text-gray-30 flex-shrink-0 group-betterhover:hover:text-gray-70" />
-        Szukaj
-=======
         <IconSearch className="mr-3 align-middle text-gray-30 shrink-0 group-betterhover:hover:text-gray-70" />
-        Search
->>>>>>> 6bd09fe682e18ccd7747fcd7798fa8fb4d3edc42
+        Szukaj
         <span className="ml-auto hidden sm:flex item-center">
           <Kbd>⌘</Kbd>
           <Kbd>K</Kbd>
